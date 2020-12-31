8d6ded6ed071dc42c6c6bfee27384e2152d23090
Nikolay Diahovets <nikolay.diahovets@gmail.com>
2021-01-01 00:59:58 +0200

    initial commit;

---
+++ Hazel/src/Hazel/Application.cpp
@@ -0,0 +1,17 @@
+#include "Application.h"
+#include <cstdio>
+
+namespace Hazel {
+    Application::Application() {
+
+    }
+
+    Application::~Application() {
+
+    }
+
+    void Application::Run() {
+        printf("Welcome to Hazel!");
+        while (true);
+    }
+}
