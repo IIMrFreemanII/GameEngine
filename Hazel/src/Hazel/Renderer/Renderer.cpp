a4a4385727063be4e1fc6b292badf03d8dfea461
Nikolay Diahovets <nikolay.diahovets@gmail.com>
2021-01-06 13:57:56 +0200

    formatting

--- Hazel/src/Hazel/Renderer/Renderer.cpp
+++ Hazel/src/Hazel/Renderer/Renderer.cpp
@@ -1,11 +1,13 @@
 #include "Renderer.h"
 
-namespace Hazel {
-    Renderer::SceneData* Renderer::s_SceneData  = new Renderer::SceneData;
+namespace Hazel
+{
+
+    Renderer::SceneData* Renderer::s_SceneData = new Renderer::SceneData;
 
     void Renderer::BeginScene(OrthographicCamera& camera)
     {
-        s_SceneData ->ViewProjectionMatrix = camera.GetViewProjectionMatrix();
+        s_SceneData->ViewProjectionMatrix = camera.GetViewProjectionMatrix();
     }
 
     void Renderer::EndScene()
@@ -15,7 +17,7 @@ namespace Hazel {
     void Renderer::Submit(const std::shared_ptr<Shader>& shader, const std::shared_ptr<VertexArray>& vertexArray)
     {
         shader->Bind();
-        shader->UploadUniformMat4("u_ViewProjection", s_SceneData ->ViewProjectionMatrix);
+        shader->UploadUniformMat4("u_ViewProjection", s_SceneData->ViewProjectionMatrix);
 
         vertexArray->Bind();
         RenderCommand::DrawIndexed(vertexArray);
