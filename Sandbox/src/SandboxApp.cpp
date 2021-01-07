7445956e67c388414c071cfd2b897788c8518c1c
Nikolay Diahovets <nikolay.diahovets@gmail.com>
2021-01-07 13:08:15 +0200

    Fixed single warning.

--- Sandbox/src/SandboxApp.cpp
+++ Sandbox/src/SandboxApp.cpp
@@ -131,9 +131,12 @@ public:
 
 			layout(location = 0) in vec3 a_Position;
 			layout(location = 1) in vec2 a_TexCoord;
+
 			uniform mat4 u_ViewProjection;
 			uniform mat4 u_Transform;
+
 			out vec2 v_TexCoord;
+
 			void main()
 			{
 				v_TexCoord = a_TexCoord;
@@ -145,9 +148,11 @@ public:
 			#version 330 core
 
 			layout(location = 0) out vec4 color;
+
 			in vec2 v_TexCoord;
 
 			uniform sampler2D u_Texture;
+
 			void main()
 			{
 				color = texture(u_Texture, v_TexCoord);
